b12862b93b92aa0b7e4c8fe7ce44227cbe1cbecb
Elmar Wachtmeester <elmar@wachtmeestertjes.nl>
2024-12-07 22:00:37 +0100

    Added maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.25"
     application
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "nl.w8mr"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 
 tasks.test {
@@ -25,6 +25,38 @@ kotlin {
     jvmToolchain(21)
 }
 
-application {
-    mainClass.set("MainKt")
+mavenPublishing {
+    coordinates(artifactId = "parsek")
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    pom {
+        name.set("Parsek")
+        description.set("The parser combinator for (and written in) Kotlin")
+        inceptionYear.set("2024")
+        url.set("https://github.com/w8mr/parsek")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/license/mit")
+                distribution.set("https://opensource.org/license/mit")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("w8mr")
+                name.set("Elmar Wachtmeester")
+                url.set("https://github.com/w8mr")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/w8mr/parsek/")
+            connection.set("scm:git:git://github.com/w8mr/parsek.git")
+            developerConnection.set("scm:git:ssh://git@github.com:w8mr/parsek.git")
+        }
+    }
+
+    signAllPublications()
 }
