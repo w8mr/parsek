e4430e192db6bd4dbbd6a13dac4446440d069e8c
Elmar Wachtmeester <elmar@wachtmeestertjes.nl>
2025-03-03 23:33:46 +0100

    Beter handling of LiteralParser

--- lib/src/commonMain/kotlin/nl/w8mr/parsek/text/Seq.kt
+++ lib/src/commonMain/kotlin/nl/w8mr/parsek/text/Seq.kt
@@ -1,8 +1,12 @@
 package nl.w8mr.parsek.text
 
-import nl.w8mr.parsek.Parser
-import nl.w8mr.parsek.seq
+import nl.w8mr.parsek.*
 
 infix fun <R> Char.and(other: Parser<Char, R>) = seq(literal(this), other)
 infix fun <R> Parser<Char, R>.and(other: Char) = seq(this, literal(other))
 infix fun Parser<Char, String>.and(other: Parser<Char, String>) = seq(this, other) { a, b -> "$a$b"}
+
+fun any(parser: LiteralParser<Char>): LiteralParser<Char> = literalCombi {
+    -repeat(parser)
+    Unit
+}
