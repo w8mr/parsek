6bbbae7f4c5a2f46f6fd289f247acf6e37c6475a
Elmar Wachtmeester <elmar@wachtmeestertjes.nl>
2025-03-03 13:45:03 +0100

    Updated JsonTest

--- lib/src/commonTest/kotlin/nl/w8mr/parsek/JsonTest.kt
+++ lib/src/commonTest/kotlin/nl/w8mr/parsek/JsonTest.kt
@@ -3,20 +3,20 @@ package nl.w8mr.parsek
 import io.kotest.matchers.shouldBe
 import nl.w8mr.parsek.text.*
 import kotlin.test.Test
-import nl.w8mr.parsek.text.zeroOrMore
+import nl.w8mr.parsek.text.any
 import or
 
 class JsonTest {
     @Test
     fun `basic json`() {
         val parser = object {
-            val ws = zeroOrMore(' ' or '\t' or '\n')
-            val quotesString = seq(char('"'), zeroOrMore(char { it != '"'}),  char("")) { _, s, _ -> s }//val quotesString = '"' and zeroOrMore(char { it != '"'}) and '"'
-            val key = quotesString
+            val ws = any(' ' or '\t' or '\n')
+            val quotesString = '"' and any(char { it != '"'}) and '"'
+            val key = seq(ws, quotesString, ws) { _, k, _-> k }
             val obj: Parser<Char, Any> = combi {
                 -ws
                 -char('{')
-                val properties = -zeroOrMore(seq(seq(seq(ws, key, ws, char(":")) { _, k, _, _ -> k } , seq(ws, ref(::value), ws) { _, v, _ -> v }), ws, repeat(char(','),1,0), ws) { p, _, _, _ -> p})
+                val properties = -zeroOrMore(seq(seq(key and ':' , seq(ws, ref(::value), ws) { _, v, _ -> v }), ws, repeat(char(','),1,0), ws) { p, _, _, _ -> p})
                 -char('}')
                 properties.toMap()
 
