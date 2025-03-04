2e8c2035195009bc742dfd63edb8bbee80ac11b9
Elmar Wachtmeester <elmar@wachtmeestertjes.nl>
2025-03-04 09:48:33 +0100

    asLiteral / any(Literal)

--- lib/src/commonMain/kotlin/nl/w8mr/parsek/text/Seq.kt
+++ lib/src/commonMain/kotlin/nl/w8mr/parsek/text/Seq.kt
@@ -6,7 +6,4 @@ infix fun <R> Char.and(other: Parser<Char, R>) = seq(literal(this), other)
 infix fun <R> Parser<Char, R>.and(other: Char) = seq(this, literal(other))
 infix fun Parser<Char, String>.and(other: Parser<Char, String>) = seq(this, other) { a, b -> "$a$b"}
 
-fun any(parser: LiteralParser<Char>): LiteralParser<Char> = literalCombi {
-    -repeat(parser)
-    Unit
-}
+fun any(parser: LiteralParser<Char>): LiteralParser<Char> = repeat(parser).asLiteral()
